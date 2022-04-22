b6681e0fc4de8ff27df36fa3005137ac6d55a279
Wayan Wiprayoga <iwawiwi@gmail.com>
2022-04-22 10:39:47 +0800

    - lrw-without-pain adaptation to lightning - not reproducible yet, need fix!

---
+++ src/datamodules/lrw_datamodule.py
@@ -0,0 +1,99 @@
+from typing import Optional
+
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset, random_split
+
+from src.datamodules.components.lrw_dataset import LRWDataset
+
+
+class LRWDataModule(LightningDataModule):
+    """
+    Example of LightningDataModule for HMDB51 dataset.
+
+    A DataModule implements 5 key methods:
+        - prepare_data (things to do on 1 GPU/TPU, not on every GPU/TPU in distributed mode)
+        - setup (things to do on every accelerator in distributed mode)
+        - train_dataloader (the training dataloader)
+        - val_dataloader (the validation dataloader(s))
+        - test_dataloader (the test dataloader(s))
+
+    This allows you to share a full dataset without explaining how to download,
+    split, transform and process the data.
+
+    Read the docs:
+        https://pytorch-lightning.readthedocs.io/en/latest/extensions/datamodules.html
+    """
+
+    def __init__(
+        self,
+        data_dir: str = "data/lrw_crop",
+        label_path: str = "data/lrw_crop/label_sorted.txt",
+        batch_size: int = 16,
+        num_workers: int = 4,
+        pin_memory: bool = True,
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        self.save_hyperparameters(logger=False)
+
+        # data transformations
+        self.train_transforms = None
+        self.test_transforms = None
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+    @property
+    def num_classes(self) -> int:
+        return 500
+
+    def prepare_data(self):
+        """Download data if needed. This method is called only from a single GPU.
+        Do not use it to assign state (self.x = y)."""
+        pass
+
+    def setup(self, stage: Optional[str] = None):
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+        This method is called by lightning twice for `trainer.fit()` and `trainer.test()`, so be careful if you do a random split!
+        The `stage` can be used to differentiate whether it's called before trainer.fit()` or `trainer.test()`."""
+
+        # transform already defined in LRWDataset class
+        if not self.train_transforms and not self.test_transforms:
+            self.train_transforms = None
+            self.test_transforms = None
+
+        # load datasets only if they're not loaded already
+        if not self.data_train and not self.data_val and not self.data_test:
+            # load all training samples
+            self.data_train = LRWDataset(self.hparams.data_dir, self.hparams.label_path, "train")
+            self.data_val = LRWDataset(self.hparams.data_dir, self.hparams.label_path, "val")
+            self.data_test = LRWDataset(self.hparams.data_dir, self.hparams.label_path, "test")
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
