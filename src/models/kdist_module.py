86adeaf2c325334516112ddaf774c11491a10953
Wayan Wiprayoga <iwawiwi@gmail.com>
2022-04-29 12:59:27 +0800

    knowledge distillation experiment minor issue: student model save teacher model too

--- src/models/kdist_module.py
+++ src/models/kdist_module.py
@@ -1,18 +1,15 @@
-from turtle import forward
 import torch
 import torch.nn as nn
-
+from pytorch_lightning import LightningModule
 from torchmetrics import MaxMetric
 from torchmetrics.classification.accuracy import Accuracy
 
 from src.models.components.cnnmodel_factory import CnnModelFactory
 
-from pytorch_lightning import LightningModule
 
-
-class StudentTrainingModule(LightningModule):
+class TeacherTrainingModule(LightningModule):
     """
-    Example of LightningModule
+    Example of LightningModule for Teacher model
 
     A LightningModule organizes your PyTorch code into 5 sections:
         - Computations (init).
@@ -24,12 +21,13 @@ class StudentTrainingModule(LightningModule):
     Read the docs:
         https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
     """
+
     def __init__(
         self,
-        student_model: str = "densenet121",
-        image_size: int = 244, 
-        num_classes: int = 10, 
-        lr: float = 0.001, 
+        teacher_model_name: str = "densenet121",
+        image_size: int = 244,
+        num_classes: int = 10,
+        lr: float = 0.001,
         weight_decay: float = 0.0005,
         pretrained=False,
     ):
@@ -37,9 +35,9 @@ class StudentTrainingModule(LightningModule):
         # this line allows to access init params with 'self.hparams' attribute
         # it also ensures init params will be stored in ckpt
         self.save_hyperparameters(logger=False)
-        
+
         # model
-        self.model = self.load_model(self.hparams.student_model)
+        self.model = self.load_model(self.hparams.teacher_model_name)
 
         # loss function
         self.criterion = nn.CrossEntropyLoss()
@@ -56,10 +54,10 @@ class StudentTrainingModule(LightningModule):
     def load_model(self, model_name):
         model_factory = CnnModelFactory()
         return model_factory.build_model(
-            model_name=model_name, 
+            model_name=model_name,
             image_size=self.hparams.image_size,
             num_classes=self.hparams.num_classes,
-            pretrained=self.hparams.pretrained
+            pretrained=self.hparams.pretrained,
         )
 
     def forward(self, x):
@@ -85,7 +83,7 @@ class StudentTrainingModule(LightningModule):
         preds = self.forward(images)
         loss = self.criterion(preds, labels)
         acc = (preds.argmax(-1) == labels).float()
-       
+
         # log val metrics
         acc = self.val_acc(preds, labels)
         self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
@@ -106,44 +104,91 @@ class StudentTrainingModule(LightningModule):
 
     def configure_optimizers(self):
         return torch.optim.SGD(
-            self.parameters(), lr=self.hparams.lr, momentum=0.9, weight_decay=self.hparams.weight_decay,
+            self.model.parameters(),
+            lr=self.hparams.lr,
+            momentum=0.9,
+            weight_decay=self.hparams.weight_decay,
         )
 
 
-class DistilledTrainingModule(StudentTrainingModule):
+class StudentTrainingModule(LightningModule):
+    """
+    Example of LightningModule
+
+    A LightningModule organizes your PyTorch code into 5 sections:
+        - Computations (init).
+        - Train loop (training_step)
+        - Validation loop (validation_step)
+        - Test loop (test_step)
+        - Optimizers (configure_optimizers)
+
+    Read the docs:
+        https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
+    """
+
     def __init__(
-        self, 
-        student_model, 
-        teacher_model,
-        image_size: int = 244, 
-        num_classes: int = 10, 
-        lr: float = 0.001, 
+        self,
+        teacher_model_path: str = "",
+        student_model_name: str = "densenet121",
+        image_size: int = 244,
+        num_classes: int = 10,
+        lr: float = 0.001,
         weight_decay: float = 0.0005,
         pretrained=False,
     ):
+        super().__init__()
         # this line allows to access init params with 'self.hparams' attribute
         # it also ensures init params will be stored in ckpt
         self.save_hyperparameters(logger=False)
 
-        super().__init__(student_model=student_model, image_size=image_size, num_classes=num_classes, lr=lr, weight_decay=weight_decay, pretrained=pretrained)
+        # model
+        self.model = self.load_model(self.hparams.student_model_name)
+        # load teacher model
+        self.teacher = TeacherTrainingModule.load_from_checkpoint(teacher_model_path).model
+
+        # loss function
+        self.criterion = nn.CrossEntropyLoss()
         self.distill_loss = nn.MSELoss()
-        self.teacher_model = self.load_model(self.hparams.teacher_model)
-        # load saved weights
-        self.teacher_model.load_model()
+
+        # use separate metric instance for train, val and test step
+        # to ensure a proper reduction over the epoch
+        self.train_acc = Accuracy()
+        self.val_acc = Accuracy()
+        self.test_acc = Accuracy()
+
+        # for logging best so far validation accuracy
+        self.val_acc_best = MaxMetric()
+
         # freeze teacher weights
-        for param in self.teacher_model.parameters():
+        for param in self.teacher.parameters():
             param.requires_grad = False
+
+    def load_model(self, model_name):
+        model_factory = CnnModelFactory()
+        return model_factory.build_model(
+            model_name=model_name,
+            image_size=self.hparams.image_size,
+            num_classes=self.hparams.num_classes,
+            pretrained=self.hparams.pretrained,
+        )
+
+    def forward(self, x):
+        return self.model(x)
 
     def training_step(self, batch, batch_idx):
         images, labels = batch
-        
-        student_pred = self.forward(images)                     # student prediction
-        teacher_pred = self.teacher_model.forward(images)       # teacher prediction
 
-        loss = self.distill_loss(student_pred, teacher_pred)    # loss based on mse loss between student_pred and teacher_pred 
+        student_pred = self.forward(images)  # student prediction
+        teacher_pred = self.teacher.forward(images)  # teacher prediction
+
+        loss = self.distill_loss(
+            student_pred, teacher_pred
+        )  # loss based on mse loss between student_pred and teacher_pred
 
         # log train metrics
-        acc = self.train_acc(student_pred, labels)              # accuracy of student based on true target label
+        acc = self.train_acc(
+            student_pred, labels
+        )  # accuracy of student based on true target label
         self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
         self.log("train/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
 
@@ -151,3 +196,35 @@ class DistilledTrainingModule(StudentTrainingModule):
         # and then read it in some callback or in `training_epoch_end()`` below
         # remember to always return loss from `training_step()` or else backpropagation will fail!
         return {"loss": loss, "preds": student_pred, "targets": labels}
+
+    def validation_step(self, batch, batch_idx):
+        images, labels = batch
+        preds = self.forward(images)
+        loss = self.criterion(preds, labels)
+        acc = (preds.argmax(-1) == labels).float()
+
+        # log val metrics
+        acc = self.val_acc(preds, labels)
+        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+        return {"loss": loss, "preds": preds, "targets": labels}
+
+    def test_step(self, batch, batch_idx):
+        """
+        No test step defined
+        """
+        pass
+
+    def validation_epoch_end(self, outputs):
+        acc = self.val_acc.compute()  # get val accuracy from current epoch
+        self.val_acc_best.update(acc)
+        self.log("val/acc_best", self.val_acc_best.compute(), on_epoch=True, prog_bar=True)
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(
+            self.model.parameters(),
+            lr=self.hparams.lr,
+            momentum=0.9,
+            weight_decay=self.hparams.weight_decay,
+        )
